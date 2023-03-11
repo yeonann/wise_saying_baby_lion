a254910a003fb59efde06f50a2375cea6e097572
yeonann <lkim82@naver.com>
2023-03-11 23:12:07 +0900

    3단계

--- src/main/java/org/io/babyLion/wiseSaying/App.java
+++ src/main/java/org/io/babyLion/wiseSaying/App.java
@@ -24,6 +24,8 @@ public class App {
                 String content = sc.nextLine().trim();
                 System.out.print("작가 : ");
                 String author = sc.nextLine().trim();
+
+                System.out.println("1번 명언이 등록되었습니다.");
             }
 
         }
